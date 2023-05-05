d728f4bb8fdb89d3301646f2dee3a3f649697fae
Brian Chapman <chapman.brian86@gmail.com>
2023-05-05 09:58:47 -0400

    Started building out components, beginning with the Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Navbar from "../components/Navbar";
+import ProductGrid from "../components/ProductGrid";
+import Skelton from "../components/Skelton";
 
 const Home: NextPage = () => {
+    const getAllCategories = async () => {
+        try {
+            const respJSON = await fetch("/api/categories");
+            const resp = await respJSON.json();
+            return resp;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const { isLoading, data } = useQuery(
+        ["AllCateforiesWithProducts"],
+        getAllCategories
+    );
+
+    const categories = data?.categories;
+
     return (
-        <div>
+        <div> 
             <Head>
-                <title>All Products</title>
-                <meta name="description" content="All Products" />
-                <link rel="icon" href="/favicon.ico" />
+            <title>All Products</title>
+            <meta name="description" content="All Products"/>
+            <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className="container mx-auto">
-                <h1 className="h-1">Hello</h1>
+                <Navbar />
+                { isLoading ? (
+                    <Skelton />
+                ) : (
+                    <>
+                    { categories && categories.length > 0 && (
+                        <ProductGrid
+                        showLink = {true}
+                        categories = {categories}
+                        />
+                    )}; 
+                    </>
+                )}
             </main>
         </div>
     );
