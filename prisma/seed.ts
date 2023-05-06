a6856b798c8f8457b66d1c994eaa43428a592e16
Brian Chapman <chapman.brian86@gmail.com>
2023-05-06 16:46:44 -0400

    Debugging. Having issues rendering product grid. Starting with schema and seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,60 +1,60 @@
-// import {
-//     randBetweenDate,
-//     randNumber,
-//     randProduct,
-//     randProductAdjective,
-// } from "@ngneat/falso";
-// import { PrismaClient } from "@prisma/client";
+import {
+    randBetweenDate,
+    randNumber,
+    randProduct,
+    randProductAdjective,
+} from "@ngneat/falso";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const primsa = new PrismaClient();
 
-// const main = async () => {
-//     try {
-//         await prisma.category.deleteMany();
-//         await prisma.product.deleteMany();
-//         const mockProducts = randProduct({
-//             length: 25,
-//         });
-//         for (let index = 0; index < mockProducts.length; index++) {
-//             const product = mockProducts[index];
-//             const productAdjective = randProductAdjective();
-//             await prisma.product.upsert({
-//                 where: {
-//                     title: `${ productAdjective } ${ product.title }`,
-//                 },
-//                 create: {
-//                     title: `${ productAdjective } ${ product.title }`,
-//                     description: product.description,
-//                     price: product.price,
-//                     image: `${ product.image }/tech`,
-//                     quantity: randNumber({ min: 10, max: 1,
-//                     category: {
-//                         connectOrCreate: {
-//                             where: {
-//                                 name: product.category,
-//                             },
-//                             create: {
-//                                 name: product.category,
-//                                 createdAt: randBetweenDate({
-//                                     from: new Date("4/29/2023"),
-//                                     to: new Date(),
-//                                 }),
-//                             },
-//                         },
-//                     },
-//                     createdAt: randBetweenDate({
-//                         from: new Date("4/30/2023"),
-//                         to: new Date(),
-//                     }),
-//                 },
-//                 update: {},
-//             });
-//         }
-//     } catch (error) {
-//         throw error;
-//     }
-// };
+const main = async () => {
+    try {
+        await primsa.category.deleteMany();
+        await primsa.product.deleteMany();
+        const fakeProducts = randProduct({
+            length: 1000,
+        });
+        for (let index = 0; index < fakeProducts.length; index++) {
+            const product = fakeProducts[index];
+            const productAdjective = randProductAdjective();
+            await primsa.product.upsert({
+                where: {
+                    title: `${productAdjective} ${product.title}`,
+                },
+                create: {
+                    title: `${productAdjective} ${product.title}`,
+                    description: product.description,
+                    price: product.price,
+                    image: `${product.image}/tech`,
+                    quantity: randNumber({ min: 10, max: 100 }),
+                    category: {
+                        connectOrCreate: {
+                            where: {
+                                name: product.category,
+                            },
+                            create: {
+                                name: product.category,
+                                createdAt: randBetweenDate({
+                                    from: new Date("10/06/2020"),
+                                    to: new Date(),
+                                }),
+                            },
+                        },
+                    },
+                    createdAt: randBetweenDate({
+                        from: new Date("10/07/2020"),
+                        to: new Date(),
+                    }),
+                },
+                update: {},
+            });
+        }
+    } catch (error) {
+        throw error;
+    }
+};
 
-// main().catch((err) => {
-//     console.warn("Error while generating seed: \n", err);
-// })
+main().catch((err) => {
+    console.warn("Error While generating Seed: \n", err);
+});
