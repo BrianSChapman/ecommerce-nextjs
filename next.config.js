af31780ff9def9e891db28daaf7f48a3392dd178
Brian Chapman <chapman.brian86@gmail.com>
2023-05-23 14:07:55 -0400

    Trying to rework image sourcing because placeimg.com will be taken down next month

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placeimg.com',
-      }
-    ]
+   domains: ['placeimg.com', 'pixabay.com', 'unsplash.com']
   },
 };
 
